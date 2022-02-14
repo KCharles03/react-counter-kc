f126eae58019c0e18253907e2bd8f565bf5922d7
“Keren <kerencharles4@gmail.com>
2022-02-14 11:46:37 -0500

    coffee-list

--- src/Coffees.js
+++ src/Coffees.js
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from 'react';
 
 function Coffees() {
     const [coffeeList, setCoffeeList] = useState();
+    const [temperature, setTemperature] = useState('hot');
     useEffect(() => {
-        fetch('https://api.sampleapis.com/coffee/hot')
+        fetch(`https://api.sampleapis.com/coffee/${temperature}`)
             .then(response => response.json())
             .then(data => setCoffeeList(data))
             .catch(err => console.error(err))
-    }, [])
+    }, [temperature])
     return (
         <section className='coffee-counter' id="coffees">
             <h2>Coffee Recipes</h2>
+            <button onClick={() => setTemperature('hot')}>Hot</button> <button onClick={() => setTemperature('iced')}>Iced</button>
             {!coffeeList
                 ? <h3>Loading..</h3>
                 : <ol>{coffeeList.map(coffee => {
